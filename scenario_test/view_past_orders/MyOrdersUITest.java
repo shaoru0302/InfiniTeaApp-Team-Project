b0010ff3dd69621f02143ffd0cdc6de8874a27a0
Shaoru Wu-Zhu <shaoruwuzhu@gmail.com>
2025-04-19 19:44:59 -0400

    Espressso Test for user story #View and Reorders, scenario #access My Orders (RED)

---
+++ scenario_test/view_past_orders/MyOrdersUITest.java
@@ -0,0 +1,42 @@
+// Scenario 1 Access MyOrders from homePage: RED – UI Test for access past orders
+package com.example.myorderfeature;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.espresso.intent.Intents;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MyOrdersUITest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    // Scenario 1: RED test
+    @Test
+    public void testAccessPastOrders() {
+        onView(withId(R.id.btn_my_orders)).perform(click());
+        onView(withId(R.id.orders_recycler_view)).check(matches(isDisplayed()));
+    }
+}
